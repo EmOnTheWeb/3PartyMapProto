5fff65ba02a8b26872cbe5bd3cabf8a68412ba67
Emilie-Purr <emiliedannenberg@purrdigital.com>
2017-01-01 18:09:19 +0000

    add launch navigator plugin and some styling changes

---
+++ platforms/browser/platform_www/cordova_plugins.js
@@ -0,0 +1,18 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/uk.co.workingedge.phonegap.plugin.launchnavigator/www/common.js",
+        "id": "uk.co.workingedge.phonegap.plugin.launchnavigator.Common",
+        "pluginId": "uk.co.workingedge.phonegap.plugin.launchnavigator",
+        "clobbers": [
+            "launchnavigator"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "uk.co.workingedge.phonegap.plugin.launchnavigator": "3.2.1"
+}
+// BOTTOM OF METADATA
+});
